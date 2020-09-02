f9c5b0931d1dc82bffb1587081c548901389bd31
Locke Birdsey <locke.birdsey@gmail.com>
2020-09-02 16:32:55 +0300

    now with jinja templates for nicer python.

--- the_hero/hero_server.py
+++ the_hero/hero_server.py
@@ -1,32 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from the_hero.hero import HeroDatabase
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'filesystem'
 
 
 @app.route('/')
 def show_events():
     # Display existing Database content
-    html = "<h1>Existing Feats</h1>"
-    html += "<table> <tr> <th>When I saved the world</th><th>How I saved the world</th><th>Who I thank for being so awesome</th></tr>"
-    heroDB = HeroDatabase()
-    heroDB.connect()
-    all_results = heroDB.inspect()
-    for i in all_results:
-        html += "<tr>"
-        html += "<td>" + str(i[1]) + "</td>"
-        html += "<td>" + i[2] + "</td>"
-        html += "<td>" + i[3] + "</td>"  # TODO: This needs to be parsed as a list
-        html += "</tr>"
-        html += str("\n")
-    heroDB.close()
-    return html
+    hero_db = HeroDatabase()
+    hero_db.connect()
+    all_results = hero_db.inspect()
+    hero_db.close()
+    return render_template('existingevents.html', title='Existing Events', rows=all_results)
 
 
-@app.route('add')
+class EventRegisterForm(FlaskForm):
+    time = StringField("Time", validators=[DataRequired()])
+    how = StringField("How", validators=[DataRequired()])
+    who = StringField("Who", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route('/add', methods=['GET', 'POST'])
 def add_event():
-    x = 1
+    form = EventRegisterForm()
+    if form.validate_on_submit():
+        # add to database
+        hero_db = HeroDatabase()
+        hero_db.connect()
+        the_time = form.time.data
+        the_how = form.how.data
+        the_who = form.who.data
+        hero_db.new_save(the_time, the_how, the_who)
+        hero_db.close()
+        return show_events()
+    return render_template('formadd.html', title='Add Event', form=form)
 
 
 if __name__ == '__main__':
